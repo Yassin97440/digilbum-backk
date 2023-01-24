743e3c815aa85a7aa84a1dd1fc559182d2bad4af
Yassin97440 <yassin.abdulla@compufirst.com>
2023-01-24 22:17:02 +0100

    findPictureByAlbum

--- src/main/java/com/digilbum/app/repositorys/PictureRepository.java
+++ src/main/java/com/digilbum/app/repositorys/PictureRepository.java
@@ -1,7 +1,12 @@
 package com.digilbum.app.repositorys;
 
+import com.digilbum.app.models.Album;
 import com.digilbum.app.models.Picture;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface PictureRepository extends CrudRepository<Picture, Integer> {
+    public List<Picture> findByAlbum(Album album);
 }
