1037fb98b2c5baba1b82455b17cb04d3a8b89c6a
Yassin97440 <yassin.abdulla@compufirst.com>
2023-03-05 01:09:21 +0100

    delete unused import

--- src/main/java/com/digilbum/app/services/HomeService.java
+++ src/main/java/com/digilbum/app/services/HomeService.java
@@ -1,23 +1,12 @@
 package com.digilbum.app.services;
 
 import com.digilbum.app.models.Album;
-import com.digilbum.app.repositorys.AlbumRepository;
-import com.digilbum.app.repositorys.PictureRepository;
-import com.digilbum.app.repositorys.UserRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v2")
 public class HomeService {
 
-    @Autowired
-    AlbumRepository albumRepo;
-    @Autowired
-    PictureRepository pictureRepo;
-    @Autowired
-    UserRepository userRepo;
 
     @GetMapping("/hello/{name}")
     public @ResponseBody String helloWorld(@PathVariable String name) {
--- src/main/java/com/digilbum/app/services/PictureService.java
+++ src/main/java/com/digilbum/app/services/PictureService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -15,8 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 import com.digilbum.app.controllers.IPictureController;
 import com.digilbum.app.dao.AlbumDao;
 import com.digilbum.app.models.Picture;
-import com.digilbum.app.repositorys.AlbumRepository;
-import com.digilbum.app.repositorys.PictureRepository;
 
 @RestController
 @RequestMapping("/pictures")
@@ -26,12 +23,6 @@ public class PictureService {
 
     @Autowired
     IPictureController pictureController;
-
-    @Autowired
-    PictureRepository pictureRepository;
-
-    @Autowired
-    AlbumRepository albumRepository;
 
     @Autowired
     AlbumDao albumDao;
