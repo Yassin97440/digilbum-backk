250a3975f841611c2dff2979cb764afd7ccdb461
Yassin97440 <yassin.abdulla@compufirst.com>
2023-03-05 01:08:23 +0100

    add file from Alibu exemple and spring security dependencies

---
+++ src/main/java/com/digilbum/app/security/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.digilbum.app.security.token;
+
+public enum TokenType {
+  BEARER
+}
---
+++ src/main/java/com/digilbum/app/security/user/Role.java
@@ -0,0 +1,7 @@
+package com.digilbum.app.security.user;
+
+public enum Role {
+
+  USER,
+  ADMIN
+}
