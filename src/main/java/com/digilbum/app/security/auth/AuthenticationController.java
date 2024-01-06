3659a853acf4c77d629b07e144a1c6249e8b9a56
Yassin97440 <yassin.abdulla@compufirst.com>
2024-01-06 19:57:24 +0100

    CORS config pour back remote et front local
    Signed-off-by: Yassin97440 <yassin.abdulla@compufirst.com>

--- src/main/java/com/digilbum/app/security/auth/AuthenticationController.java
+++ src/main/java/com/digilbum/app/security/auth/AuthenticationController.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
