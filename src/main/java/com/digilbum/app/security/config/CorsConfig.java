5093a87ad1ed14c1462fdcd07d30e9e83c617beb
Yassin97440 <yassin.abdulla@compufirst.com>
2024-04-20 00:41:45 +0200

    @Cross origin dans cors config
    Signed-off-by: Yassin97440 <yassin.abdulla@compufirst.com>

--- src/main/java/com/digilbum/app/security/config/CorsConfig.java
+++ src/main/java/com/digilbum/app/security/config/CorsConfig.java
@@ -10,8 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://159.89.0.150:3000") // Add the origin of your Nuxt application
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Add the allowed HTTP methods
+                .allowedOrigins("http://localhost:3000","http://192.168.1.1:3000") // Add the origin of your Nuxt application
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Add the allowed HTTP methods
                 .allowedHeaders("*") // Add the allowed headers
                 .allowCredentials(true)
                 .maxAge(3600); // Allow credentials (e.g., cookies, authorization headers)
