928e38215d57c2fe2d2698dba8d8ec6ca43d5ace
Yassin97440 <yassin.abdulla@compufirst.com>
2024-04-22 22:08:46 +0200

    add buidler and @createdDate
    Signed-off-by: Yassin97440 <yassin.abdulla@compufirst.com>

--- src/main/java/com/digilbum/app/models/Group.java
+++ src/main/java/com/digilbum/app/models/Group.java
@@ -1,12 +1,15 @@
 package com.digilbum.app.models;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.Instant;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Entity
@@ -26,6 +29,7 @@ public class Group {
     @Column(name = "Name", nullable = false, length = 150)
     private String name;
 
+    @CreatedDate
     @Column(name = "CreatedAt")
     private Instant createdAt;
 
