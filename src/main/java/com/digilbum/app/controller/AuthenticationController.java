99bede333d27e662ec6c8b486acbe423adbd6144
Yassin97440 <yassin.abdulla@compufirst.com>
2024-11-19 23:02:12 +0100

    handling error response
    Signed-off-by: Yassin97440 <yassin.abdulla@compufirst.com>

--- src/main/java/com/digilbum/app/controller/AuthenticationController.java
+++ src/main/java/com/digilbum/app/controller/AuthenticationController.java
@@ -7,11 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/api/v1/auth")
 @CrossOrigin(origins = "http://159.89.0.150:3000")
 @RequiredArgsConstructor
 public class AuthenticationController {
+
+  Logger logger = Logger.getLogger(getClass().getCanonicalName());
 
   private final AuthenticationService service;
 
@@ -22,7 +27,9 @@ public class AuthenticationController {
       return new ResponseEntity<>(service.register(request), HttpStatus.OK);
     }
     catch (Exception e){
+      logger.log(Level.SEVERE, e.getMessage());
       return new ResponseEntity<>(new AuthenticationResponse("Erreur lors de la création de compte"), HttpStatus. INTERNAL_SERVER_ERROR);
+
     }
   }
   @PostMapping("/login")
@@ -32,10 +39,12 @@ public class AuthenticationController {
       return new ResponseEntity<>(service.authenticate(request), HttpStatus.OK);
     }
     catch (AuthenticationException e){
+      logger.log(Level.SEVERE, e.getMessage());
       return new ResponseEntity<>(new AuthenticationResponse("Authentification échouée, vérifier vos identifiants"), HttpStatus.UNAUTHORIZED);
     }
     //pas bonne pratique apparemment, à voir comment mieux gérer plusieurs exceptions. avec instanceOf ?
     catch (RuntimeException e){
+      logger.log(Level.SEVERE, e.getMessage());
       return new ResponseEntity<>(new AuthenticationResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
