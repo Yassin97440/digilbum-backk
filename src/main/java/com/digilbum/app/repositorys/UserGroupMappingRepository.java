8542a3375b5b24dabc76d62a3e46b4e372a6a734
Yassin97440 <yassin.abdulla@compufirst.com>
2024-12-21 01:35:45 +0100

    feat/also for get shared albums for user
    Signed-off-by: Yassin97440 <yassin.abdulla@compufirst.com>

--- src/main/java/com/digilbum/app/repositorys/UserGroupMappingRepository.java
+++ src/main/java/com/digilbum/app/repositorys/UserGroupMappingRepository.java
@@ -4,7 +4,10 @@ import com.digilbum.app.models.UserGroupMapping;
 import com.digilbum.app.models.UserGroupMappingId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserGroupMappingRepository
         extends JpaRepository<UserGroupMapping, UserGroupMappingId>
 {
+    List<UserGroupMapping> findById_User_Id(Integer id);
 }
