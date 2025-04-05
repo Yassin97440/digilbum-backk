96efa312ca3a90da0826872536ac4d0e35473fc0
Yassin97440 <yassin.abdulla@compufirst.com>
2025-04-05 20:46:53 +0200

    refacto : move exception

--- src/main/java/com/digilbum/app/security/auth/AuthorizationService.java
+++ src/main/java/com/digilbum/app/security/auth/AuthorizationService.java
@@ -1,7 +1,7 @@
 package com.digilbum.app.security.auth;
 
 import com.digilbum.app.repositorys.RoleRepository;
-import com.digilbum.app.security.RoleNotFoundException;
+import com.digilbum.app.security.exception.RoleNotFoundException;
 import com.digilbum.app.security.user.Role;
 import com.digilbum.app.security.user.User;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +14,8 @@ public class AuthorizationService {
     private final RoleRepository roleRepository;
 
     public void giveUserBasicRole(User user) {
-        Role role = roleRepository.findById("ROLE_USER").
-                orElseThrow(() -> new RoleNotFoundException("Basic role not found"));
+        Role role = roleRepository.findById("ROLE_USER")
+                .orElseThrow(() -> new RoleNotFoundException("Basic role not found"));
         user.getAuthorities().add(role);
     }
 }
